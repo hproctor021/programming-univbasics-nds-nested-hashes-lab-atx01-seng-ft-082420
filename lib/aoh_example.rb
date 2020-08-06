fe0f8e29e009d02c78849759a75e70a3c711c7dd
Haley Proctor <hproctor021@gmail.com>
2020-08-06 13:37:51 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -26,6 +26,6 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  aoh[row][key] = new_value
-  return aoh[row][key]
+  aoh[row][:key] = new_value
+  return aoh[row][:key]
 end
